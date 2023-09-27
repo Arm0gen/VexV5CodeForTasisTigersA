4437e98399f27e5c3fc6d8827780cfd7822bb062
Erranundi <otherworldlyfool@gmail.com>
2023-09-27 18:59:15 +0200

    Added the robot movement system
    Alex Varga:
    
    The code added to the userControl is the basic user input system, basing on the average of the inputs of the two ticks, determining the direction and the speed of the movement.
    
    Awaits trial

--- v5_tasis_tigers_a/src/main.cpp
+++ v5_tasis_tigers_a/src/main.cpp
@@ -81,6 +81,46 @@ void usercontrol(void) {
     // update your motors, etc.
     // ........................................................................
 
+    int left_x, left_y, right_x, right_y;
+    double avg_x, avg_y, speed, angle, left_speed, right_speed,
+    curr_speed_left, curr_speed_right, increment_left, increment_right;
+
+    left_x = user_input.Axis3.position();
+    left_y = user_input.Axis3.position();
+    right_x = user_input.Axis1.position();
+    right_y = user_input.Axis2.position();
+
+    // Calculate the average displacement (avg_x, avg_y) of the two joysticks:
+    avg_x = (left_x + right_x) / 2;
+    avg_y = (left_y + right_y) / 2;
+
+    // Calculate the magnitude (speed) and direction (angle) of the displacement:
+    speed = sqrt(pow(avg_x,2) + pow(avg_y, 2));
+    angle = atan2(avg_y, avg_x); //not in radians
+    angle *= 180.0 / M_PI; // now it is in radians
+
+    // Calculate the individual motor speeds for each wheel:
+    left_speed = speed * cos(angle);  // Speed for the left wheels
+    right_speed = speed * sin(angle); // Speed for the right wheels
+
+    // Asigning the increments by which the speeds of the motor groups will increase by every gap seconds
+    // percent_increment is a value defined in robot-config.cpp and defines by what fraction of left/right_speed will current speed increase every gap seconds
+    // gap is defined in robot-config.cpp
+    increment_left = left_speed / percent_increment;
+    increment_right = right_speed / percent_increment;
+
+    curr_speed_right = curr_speed_left = 0;
+
+    while(curr_speed_left < left_speed){
+      curr_speed_right += increment_right;
+      curr_speed_left += increment_left;
+
+      wait(gap, seconds);
+    }
+    
+    RightDriveSmart.spinFor(0.2, seconds);
+    LeftDriveSmart.spinFor(0.2, seconds);
+
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
--- v5_tasis_tigers_a/src/robot-config.cpp
+++ v5_tasis_tigers_a/src/robot-config.cpp
@@ -4,17 +4,27 @@ using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
+int percent_increment = 20;
+double gap = 0.01;
+
 // A global instance of brain used for printing to the V5 Brain screen
 brain  Brain;
 
+controller user_input = controller(primary);
+
 // VEXcode device constructors
-controller Controller1 = controller(primary);
-motor LeftDriveSmart = motor(PORT1, ratio18_1, false);
-motor RightDriveSmart = motor(PORT10, ratio18_1, true);
-gyro TurnGyroSmart = gyro(Brain.ThreeWirePort.D);
-smartdrive Drivetrain= smartdrive(LeftDriveSmart, RightDriveSmart, TurnGyroSmart, 319.19, 320, 130, mm, 1);
-motor ClawMotor = motor(PORT3, ratio18_1, false);
-motor ArmMotor = motor(PORT8, ratio18_1, false);
+
+// VEXcode device constructors
+motor leftMotorFront = motor(PORT1, ratio18_1, false);
+motor leftMotorBack = motor(PORT10, ratio18_1, false);
+motor_group LeftDriveSmart = motor_group(leftMotorFront, leftMotorBack);
+
+motor rightMotorFront = motor(PORT11, ratio18_1, true); 
+motor rightMotorBack = motor(PORT20, ratio18_1, true); 
+motor_group RightDriveSmart = motor_group(rightMotorFront, rightMotorBack);
+
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 165, mm, 1);
+
 
 // VEXcode generated functions
 
